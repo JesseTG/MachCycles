6749a2644a2064d1d6dc96311df010c702184d12
conscou <scoullisconstantina@gmail.com>
2014-09-20 19:20:22 -0400

    Merge

--- desktop/src/com/corundumgames/mach/desktop/DesktopLauncher.java
+++ desktop/src/com/corundumgames/mach/desktop/DesktopLauncher.java
@@ -7,6 +7,11 @@ import com.corundumgames.mach.MachCycles;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		
+		config.width = 800;
+        config.height = 600;
+        config.foregroundFPS = 60;
+		
 		new LwjglApplication(new MachCycles(), config);
 	}
 }
