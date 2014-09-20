aae8f66f0447f3612a3b5d0ed7bd82c69bce11fc
Jesse Talavera-Greenberg <jessetalavera@aol.com>
2014-09-20 13:59:07 -0400

    Created the first screen
    -It has nothing in it yet

--- desktop/src/com/corundumgames/mach/desktop/DesktopLauncher.java
+++ desktop/src/com/corundumgames/mach/desktop/DesktopLauncher.java
@@ -5,8 +5,12 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.corundumgames.mach.MachCycles;
 
 public class DesktopLauncher {
-	public static void main (String[] arg) {
-		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new MachCycles(), config);
-	}
+    public static void main(String[] arg) {
+        LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+
+        config.width = 800;
+        config.height = 600;
+        config.foregroundFPS = 60;
+        new LwjglApplication(new MachCycles(), config);
+    }
 }
