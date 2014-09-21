b89505ce1777df1b8ec0bbe291702866d1356861
Jesse Talavera-Greenberg <jessetalavera@aol.com>
2014-09-20 20:22:03 -0400

    Redefined the units in VelocityComponent

--- core/src/com/corundumgames/mach/components/VelocityComponent.java
+++ core/src/com/corundumgames/mach/components/VelocityComponent.java
@@ -5,15 +5,18 @@ import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.utils.Json;
 import com.badlogic.gdx.utils.Json.Serializable;
 import com.badlogic.gdx.utils.JsonValue;
+import com.corundumgames.mach.systems.MotionSystem;
 
 /**
  * Given to any game entity that moves.
+ * 
+ * @see MotionSystem
  * 
  * @author jesse
  */
 public class VelocityComponent extends PooledComponent implements Serializable {
     /**
-     * Linear velocity in units per frame
+     * Linear velocity in pixels per second
      */
     public final Vector2 linear;
 
